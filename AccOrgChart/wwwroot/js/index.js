a53aaee6d3379383c72e2652d1688f1451aebbc1
Fouad Kassouf <fouad.kassouf@exquitech.com>
2022-11-01 14:21:49 +0200

    latest

--- AccOrgChart/wwwroot/js/index.js
+++ AccOrgChart/wwwroot/js/index.js
@@ -1,34 +1,68 @@
-﻿var datascource = {
+﻿var roles = [
+    { id: 1, name: 'Operations Manager (OM)' },
+    { id: 2, name: 'Project Manager (PM)' },
+    { id: 3, name: 'Construction Manager (CM)' },
+    { id: 4, name: 'Contract Manager (CtM)' },
+    { id: 5, name: 'Adm & Fin Manager' },
+    { id: 6, name: 'Chief Accountant' },
+    { id: 7, name: 'Accountant' },
+    { id: 8, name: 'Accounting Clerk' }
+];
+
+var tasks = [
+    { id: 1, name: 'Review and endorse for stamp duties and distribution of original & copies' },
+    { id: 2, name: 'Check wording, stamp duties and distribution of original & copies' },
+    { id: 3, name: 'Receive copy, review and file' },
+    { id: 4, name: 'Handles fiscal stamps. Files Client Contracts with lawyer..' },
+    { id: 5, name: 'Coordinates fiscal stamps payments. Reviews JV (Off B/S account)' },
+    { id: 6, name: 'Creates off B/S account to record full contract value)' },
+    { id: 7, name: 'Files JV' }
+   
+];
+
+
+var datascource = {
     'id': 1,
-    'name': 'Lao Lao',
-    'title': 'general manager',
+    'roleId' : 1,
+    'role': 'Operations Manager (OM)',
+    'taskId' : 1,
+    'task': 'Review and endorse for stamp duties and distribution of original & copies',
     'children': [
         {
-            'id': 2, 'name': 'Bo Miao', 'title': 'department manager',
-            'children': [{ 'id': 3, 'name': 'Li Xin', 'title': 'senior engineer' }]
+            'id': 2, 'roleId': 2, 'role': 'Project Manager (PM)', 'taskId': 2, 'task': 'Check wording, stamp duties and distribution of original & copies',
+            'children': [{ 'id': 3, 'roleId': 3, 'role': 'Construction Manager (CM)', 'taskId': 3, 'task': 'Receive copy, review and file' }]
         },
         {
-            'id': 4, 'name': 'Su Miao', 'title': 'department manager',
+            'id': 4, 'roleId': 4, 'role': 'Contract Manager (CtM)', 'taskId': 3, 'task': 'Receive copy, review and file',
             'children': [
-                { 'id': 5, 'name': 'Tie Hua', 'title': 'senior engineer' },
+                { 'id': 5, 'roleId': 5, 'role': 'Adm & Fin Manager', 'taskId': 4, 'task': 'Handles fiscal stamps. Files Client Contracts with lawyer..' },
                 {
-                    'id': 6, 'name': 'Hei Hei', 'title': 'senior engineer',
+                    'id': 6, 'roleId': 6, 'role': 'Chief Accountant', 'taskId': 5, 'task': 'Coordinates fiscal stamps payments. Reviews JV (Off B/S account)',
                     'children': [
-                        { 'id': 7, 'name': 'Pang Pang', 'title': 'engineer' },
-                        { 'id': 8, 'name': 'Dan Dan', 'title': 'UE engineer' }
+                        { 'id': 7, 'roleId': 7, 'role': 'Accountant', 'taskId': 6, 'task': 'Creates off B/S account to record full contract value)' },
+                        { 'id': 8, 'roleId': 8, 'role': 'Accounting Clerk', 'taskId': 7, 'task': 'Files JV' }
                     ]
                 }
             ]
-        },
-        { 'id': 9, 'name': 'Hong Miao', 'title': 'department manager' }
+        }
     ]
 };
-
 $(document).ready(function () {
+
+    roles.forEach(el => {
+        $('#ddlRoles').append('<option value="' + el.id + '">' + el.name + '</option>');
+    });
+
+    tasks.forEach(el => {
+        $('#ddlTasks').append('<option value="' + el.id + '">' + el.name + '</option>');
+    });
+ 
     var oc = $('#chart-container').orgchart({
         'data': datascource,
-        'nodeContent': 'title',
+        'nodeTitle' : 'role',
+        'nodeContent': 'task',
         'nodeID': 'id',
+        'parentNodeSymbol': '',
         'draggable': true,
         'dropCriteria': function ($draggedNode, $dragZone, $dropZone) {
             if ($draggedNode.find('.content').text().indexOf('manager') > -1 && $dropZone.find('.content').text().indexOf('engineer') > -1) {
@@ -37,8 +71,14 @@ $(document).ready(function () {
             return true;
         },
         'createNode': function (node, data) {
-            $(node).click(function () {
-                console.log(data);
+            $(node).dblclick(function () {
+                
+                $('#spanModalContentTitle').text(data.role);
+                $('#ddlRoles').val(data.roleId);
+                $('#ddlTasks').val(data.taskId);
+                $('#txtTaskDesc').val(data.task);
+                $('#modalContent').modal('show');
+                
             });
 
         }
@@ -52,3 +92,16 @@ $(document).ready(function () {
     });
 
 });
+
+function changeTaskDesc(sender) {
+    
+    let checked = $(sender).prop('checked');
+    
+    $('#txtTaskDesc').prop('readonly', !checked);
+    
+
+}
+
+function onTaskChange(sender) {
+    $('#txtTaskDesc').val(sender.options[sender.selectedIndex].text);
+}
